5fc607c13568c6f69e3358f3e05bee9eb63f4b90
JotaroE21 <jusseaumejonathan5@gmail.com>
2022-11-24 19:27:30 +0100

    Port custom

--- front/src/http-common.js
+++ front/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://localhost:" + process.env["REACT_APP_API_PORT"] + "/",
   headers: {
     "Content-type": "application/json"
   }
